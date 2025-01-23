a5b60b644aa30587b9ffec1c4031395a2a5bf0d6
erikamc99 <erikamc99@gmail.com>
2025-01-23 11:30:16 +0100

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ColorPicker from './components/ColorPicker'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-       
-      </div>
+      <ColorPicker />
     </>
   )
 }
